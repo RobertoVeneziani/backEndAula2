db02ccb94ec5e717913ab6297a17ec93b7d7d4c8
RobertoVeneziani <robertovenezianicelular@hotmail.com>
2024-03-06 21:36:21 -0300

    atualizar

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Cliente from "./Modelo/Cliente.js";
 
-
+//
 const cliente = new Cliente(0, "123.456.789-10",
     "Jose Alcantara de Moraes", "Rua das Larangeiras, 213",
     "Vila ocidental", "São Paulo", "SP", "(11)92556-2235",
