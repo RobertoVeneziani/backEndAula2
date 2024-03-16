7ae4f7dcfe1623ed8cf40ca882f4b3a6fba2d498
RobertoVeneziani <robertovenezianicelular@hotmail.com>
2024-03-16 10:25:19 -0300

    atualizando

--- Controle/cliente Ctrl.js
+++ Controle/cliente Ctrl.js
@@ -49,6 +49,7 @@ export default class clienteCtrl{
         resposta.type('application/json');
         if((requsicao.method === "PATCH" || requisicao.method === "PUT") && requisicao.i('application/json')){
             const dados = requisicao.body;
+            const codigo = requisicao.params.codigo;
             const cpf = dados.cpf;
             const nome = dados.nome;
             const endereco = dados.endereco;
@@ -57,7 +58,41 @@ export default class clienteCtrl{
             const estado = dados.estado;
             const telefone = dados.telefone;
             const email = dados.email;
+            if (codigo && codigo > 0 && cpf && nome && endereco && bairro && cidade && estado && telefone && email)
+            {
+                const  cliente = new Cliente(codigo, cpf, nome, endereco, bairro, cidade, estado, telefone, email);
+                cliente.atualizar()
+                .then(()=>{
+                    resposta.status(200);
+                    resposta.json({
+                        "status": true,
+                        "mensagem":"Cliente Atualizado com Sucesso!",
+                    })
+                })
+                .catch((erro)=>{
+                    resposta.status(500)        
+                    resposta.json({     
+                        "status":false,
+                        "mensagem": "Não foi possivel atualizar cliente!" + erro.message
+                    })
+                });
+            }
+            else{
+                resposta.status(400);
+                resposta.json({
+                    "status": false,
+                    "mensagem": "Por favor, informe todos os dados do cliente, conforme documentação da API"
+                })  
+            }
+
         }
+        else{
+            resposta.status(405); 
+            resposta.json({
+                status: false,
+                mensagem: "Requisição inválida! Esperando o método PATCH, PUT e os dados no formato JSON para atualizar um cliente!"
+            })
+        }    
     }
     excluir(requisicao, resposta) {
 
