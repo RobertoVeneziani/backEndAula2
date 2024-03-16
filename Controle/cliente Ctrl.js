ac248062f80db4f2e8631eca0239c13aead827bd
RobertoVeneziani <robertovenezianicelular@hotmail.com>
2024-03-15 21:03:00 -0300

    atualização

--- Controle/cliente Ctrl.js
+++ Controle/cliente Ctrl.js
@@ -1,6 +1,7 @@
 export default class clienteCtrl{
-    gravar(requsicao, resposta){
-        if(requisicao.method === "POST" && requisicao.is('aplication/jason')){
+    gravar(requisicao, resposta){
+        resposta.type('application/json');
+        if(requisicao.method === "POST" && requisicao.is('application/json')){
             const dados = requisicao.body;
             const cpf = dados.cpf;
             const nome = dados.nome;
@@ -11,23 +12,57 @@ export default class clienteCtrl{
             const telefone = dados.telefone;
             const email = dados.email;
 
-            if (cpf && nome && endereco && bairro && cidade && estado && telefone && email){
-                const cliente = new cliente(0, cpf, nome, endereco, bairro, cidade, estado, telefone);
-            }
-            else {
-                resposta.status(400);
+        if (cpf && nome && endereco && bairro && cidade && estado && telefone && email){
+            const cliente = new cliente(0, cpf, nome, endereco, bairro, cidade, estado, telefone)};
+            cliente.gravar().then(()=>{
+                resposta.status(201);
+                resposta.json({
+                    "status": true,
+                    "mensagem":"Cliente gravado com sucesso!",
+                    "codigo_cliente": cliente.codigo
+                    });
+            }).catch((erro) =>{
+                resposta.status(500);
                 resposta.json({
                     "status": false,
-                    "mensagem": "Por favor, informe todos os dados do cliente, conforme documentação da API"
-                });
+                    "mensagem": "Não foi possível armazenar o cliente!" + erro.message
+                })
+            });
+        }
+        else {
+            resposta.status(400);
+            resposta.json({
+                "status": false,
+                "mensagem": "Por favor, informe todos os dados do cliente, conforme documentação da API"
+            });
+        }
+    } 
+    else{
+        resposta.status(405);
+        resposta.json({
+            "status": false,
+            "mensagem": "Requisição inválida! Esperando o método  POST e dados no formato JSON para gravar um cliente!"
+            }) 
+        }
     }
     atualizar(requsicao, resposta){
-
+        resposta.type('application/json');
+        if((requsicao.method === "PATCH" || requisicao.method === "PUT") && requisicao.i('application/json')){
+            const dados = requisicao.body;
+            const cpf = dados.cpf;
+            const nome = dados.nome;
+            const endereco = dados.endereco;
+            const bairro = dados.bairro;
+            const cidade = dados.cidade;
+            const estado = dados.estado;
+            const telefone = dados.telefone;
+            const email = dados.email;
+        }
     }
     excluir(requisicao, resposta) {
 
     }
     consultar(requisicao, resposta){
     }
-}
 
+}
