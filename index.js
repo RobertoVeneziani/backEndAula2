75d8d0389ab67a9019beb62502b78639202383bd
RobertoVeneziani <robertovenezianicelular@hotmail.com>
2024-03-31 16:07:05 -0300

    atualização

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import Cliente from "./Modelo/Cliente.js";
 import express from "express"
-import roraCliente from "./Rotas/rotaCliente.js"
 import rotaCliente from "./Rotas/rotaCliente.js";
+import cors  from 'cors';
 
 const host = '0.0.0.0';
 const porta = 3000;
 const app = express();
+app.use(cors({
+    origin: '*'
+    }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
